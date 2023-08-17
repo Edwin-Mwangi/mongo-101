65396168a63d1ec054391bcb7c4975c49c02dc87
Edwin-Mwangi <edwinmwangi138@gmail.com>
2023-08-17 21:35:54 +0300

    basic setup

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ const app = express();
 app.listen('3000', () => {
     console.log('listening for requests');
 })
+
+//routes
+app.get('/books', (req, res) => {
+    res.json({mssg: "Welcome to page"})
+})
