37ef03ced5c07ae3dd7a98ea89fa326c149cabdf
Edwin-Mwangi <edwinmwangi138@gmail.com>
2023-08-18 01:11:32 +0300

    fetching single doc from  mongodb

--- app.js
+++ app.js
@@ -28,3 +28,11 @@ app.get('/books', (req, res) => {
     // res.json({mssg: "Welcome to page"})
 })
 
+app.get('/books/:id', (req, res) => {
+
+    //find one book
+    db.collection('books')
+        .findOne({_id: new ObjectId(req.params.id)})
+        .then(doc => res.status(200).json(doc))
+        .catch(error => res.status(500).json({error: 'Could not fetch the document'}))
+})
