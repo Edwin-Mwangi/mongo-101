f595e195e6f0d3a512a9edcc254692cf13a3dc3f
Edwin-Mwangi <edwinmwangi138@gmail.com>
2023-08-18 01:45:40 +0300

    chacking validity of url id

--- app.js
+++ app.js
@@ -29,10 +29,16 @@ app.get('/books', (req, res) => {
 })
 
 app.get('/books/:id', (req, res) => {
+    //check validity
+    if(ObjectId.isValid(req.params.id)){
+        //find one book
+        db.collection('books')
+            .findOne({_id: new ObjectId(req.params.id)})
+            .then(doc => res.status(200).json(doc))
+            .catch(error => res.status(500).json({error: 'Could not fetch the document'}))
+    } else {
+        res.status(500).json({error: "Not a valid JSON"})
+    }
 
-    //find one book
-    db.collection('books')
-        .findOne({_id: new ObjectId(req.params.id)})
-        .then(doc => res.status(200).json(doc))
-        .catch(error => res.status(500).json({error: 'Could not fetch the document'}))
+    
 })
